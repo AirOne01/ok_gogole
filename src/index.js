85339b06f84735aa18d169f0ece357b7c6cfc0b2
AirOne01 <popGthyrd@gmail.com>
2020-03-10 23:46:22 +0100

    🌼 Implemented valid SST

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
+/*
+                                                            ▄▄
+         ▀███                                             ▀███
+           ██                                               ██
+  ▄██▀██▄  ██  ▄██▀     ▄█▀█████ ▄██▀██▄ ▄█▀█████ ▄██▀██▄   ██   ▄▄█▀██
+ ██▀   ▀██ ██ ▄█       ▄██  ██  ██▀   ▀████  ██  ██▀   ▀██  ██  ▄█▀   ██
+ ██     ██ ██▄██       ▀█████▀  ██     ███████▀  ██     ██  ██  ██▀▀▀▀▀▀
+ ██▄   ▄██ ██ ▀██▄     ██       ██▄   ▄███       ██▄   ▄██  ██  ██▄    ▄
+  ▀█████▀▄████▄ ██▄▄    ███████  ▀█████▀ ███████  ▀█████▀ ▄████▄ ▀█████▀
+                       █▀     ██        █▀     ██
+                       ██████▀          ██████▀
+              An attempt to copy Google Assistant with IBM Watson
+ */
 
+// Dependencies:
+const fs = require('fs');
+
+const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+const {IamAuthenticator} = require('ibm-watson/auth');
+
+const cfg = require("../config/config.json");
+
+// Setup SpeechToText:
+const speechToText = new SpeechToTextV1({
+    authenticator: new IamAuthenticator({apikey: cfg.apikey}),
+    url: cfg.apiurl,
+});
+
+// Params
+const params = {
+    audio: fs.createReadStream('../1.ogg'),
+    contentType: 'audio/ogg; rate=4410',
+    model: 'fr-FR_NarrowbandModel'
+};
+
+// STT test
+speechToText.recognize(params, (err, res) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(res.result.results.forEach((result) => {
+        console.log(result.alternatives)
+    }));
+}).catch(err => {
+    console.log(err);
+});
+
+console.log('Uploading the audio file and waiting..');
